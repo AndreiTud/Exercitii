5aa7ae0ff9342be41d31cde1fe2e392551b4e811
AndreiTud <andrei_t88@yahoo.com>
2013-12-11 17:03:10 +0200

    tema

--- Tema20/Tema20.dpr
+++ Tema20/Tema20.dpr
@@ -5,6 +5,37 @@ program Tema20;
 uses
   SysUtils;
 
+  const
+    Leu: integer = 100;
+    Moneda: integer = 50;
+    Bani: integer = 1;
+
+  var
+    Suma: Integer;
+    Restul: Integer;
+    NrLei: Integer;
+    NrMonezi: integer;
+    NrBani: integer;
+
 begin
+
+Writeln(output, 'Suma ce va fi descompusa : ');
+Readln(input, Suma);
+
+NrLei := Suma div Leu;
+Restul := Suma mod Leu;
+Writeln('Suma contine ' + IntToStr(NrLei) + ' bacnote de 1 leu');
+Readln;
+
+Suma := Restul;
+NrMonezi := Suma div Moneda;
+Restul := Suma mod Moneda;
+Writeln('Suma contine ' + IntToStr(NrMonezi) + ' monezi de 50 bani');
+Readln;
+
+Suma := Restul;
+NrBani := Suma div Bani;
+Writeln('Suma contine ' + IntToStr(NrBani) + ' monezi de 1 ban');
+Readln;
 
 end.
